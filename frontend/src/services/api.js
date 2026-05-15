201f0c10ea1221da621c8f21838768a84cd74ac7
Lakshay-Rewani <lakshayrewani7@gmail.com>
2026-05-15 17:36:29 +0530

    debug: add log to api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,6 +5,7 @@
 import axios from 'axios';
 
 const API_BASE = import.meta.env.VITE_API_URL || 'https://agency-ms.onrender.com/api';
+console.log('API_BASE is set to:', API_BASE);
 
 const api = axios.create({
   baseURL: API_BASE,
